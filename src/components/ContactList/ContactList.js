848f4e07af4c3450dfb014dec7d1458899d23a4a
Babak Movahhedi <babak.movahhedi2015@gmail.com>
2022-07-03 13:52:03 +0430

    final version

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,52 +1,74 @@
-import './ContactList.css';
-import { Link } from 'react-router-dom';
-import Contacts from '../Contacts/Contacts';
-import { useState,useEffect} from 'react';
-import getcontact from '../../services/getcontactservices';
-import deletecontact from '../../services/deletecontactsservices';
+import "./ContactList.css";
+import { Link } from "react-router-dom";
+import Contacts from "../Contacts/Contacts";
+import { useState, useEffect } from "react";
+import getcontact from "../../services/getcontactservices";
+import deletecontact from "../../services/deletecontactsservices";
 
 const ContactList = (props) => {
-    const[contacts,setContacts]=useState([])
-   
+  const [contacts, setContacts] = useState([]);
+  const [allContacts, setAllContacts] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const onDelete=(id)=>{
-        const filterContact=contacts.filter((c)=> c.id!== id );
-        setContacts(filterContact);
-        deletecontact(id)
-        .then((res)=>getcontact())
-        .then((res)=>setContacts(res.data))
-        .catch()
-      };
+  useEffect(() => {
+    // const newContact= JSON.parse(localStorage.getItem("contacts") );
+    // if(newContact) setContacts(newContact);
 
-    useEffect(()=>{
-        // const newContact= JSON.parse(localStorage.getItem("contacts") );
-        // if(newContact) setContacts(newContact);
-        getcontact()
-        .then((res)=>setContacts(res.data) )
-        .catch()
-      },[]);
+    getcontact()
+      .then((res) => {
+        setContacts(res.data);
+        setAllContacts(res.data);
+      })
 
-      
-    return ( 
-        <section className='listwraper'>
+      // .then((res) => console.log(res.data))
+      .catch();
+  }, []);
 
-            <div className="contactlist">
-           <div className='listHeader'>           
-           <h2>Contacts</h2>
-           <Link to='/add' >
-           <button>Add</button>
-           </Link>
-           </div  >
-           
-           {contacts.map((contact)=>{
-               return(
-                <Contacts contact={contact} onDelete={onDelete} key={contact.id} />
-               )
-           } )}
-       </div>
+  const onDelete = (id) => {
+    const filterContact = contacts.filter((c) => c.id !== id);
+    setContacts(filterContact);
+    deletecontact(id)
+      .then((res) => getcontact())
+      .then((res) => setContacts(res.data))
+      .catch();
+  };
 
-        </section>
-            );
-}
- 
+  const searchHandler = (e) => {
+    setSearchTerm(e.target.value);
+    const search = e.target.value;
+    if (search !== "") {
+      const filterContacts = allContacts.filter((c) => {
+        return Object.values(c)
+          .join("")
+          .toLowerCase()
+          .includes(search.toLowerCase());
+      });
+      setContacts(filterContacts);
+    } else {
+      setContacts(allContacts);
+    }
+  };
+
+  return (
+    <section className="listwraper">
+      <div className="contactlist">
+        <div className="listHeader">
+          <h2>Contacts</h2>
+          <Link to="/add">
+            <button>Add</button>
+          </Link>
+        </div>
+        <div>
+          <input type="text" value={searchTerm} onChange={searchHandler} />
+        </div>
+        {contacts.map((contact) => {
+          return (
+            <Contacts contact={contact} onDelete={onDelete} key={contact.id} />
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
 export default ContactList;
--- src/components/EditContact/EditContact.js
+++ src/components/EditContact/EditContact.js
@@ -1,58 +1,57 @@
 import { useState } from "react";
-import {useNavigate,useLocation,Link} from 'react-router-dom';
+import { useNavigate, useLocation, Link } from "react-router-dom";
 import getcontact from "../../services/getcontactservices";
 import putcontact from "../../services/putContactServices";
 // import './AddContact.css';
 const EditContact = () => {
-    const location=useLocation();
-    const navigate= useNavigate();
-    const[contact,setContact]=useState({name:location.state.name, email:location.state.email });
-    
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [contact, setContact] = useState({
+    name: location.state.name,
+    email: location.state.email,
+  });
 
-    const changeHandler= (e)=>{
-        setContact({...contact,[e.target.name]:e.target.value});
-    };
+  const changeHandler = (e) => {
+    setContact({ ...contact, [e.target.name]: e.target.value });
+  };
 
-    const submitform=(e)=>{
-        e.preventDefault();
-        editContactHandler(contact,location.state.id);
-        setContact({name:'',email:''});
-       navigate('/');
-        
-    };
+  const submitform = (e) => {
+    e.preventDefault();
+    editContactHandler(contact, location.state.id);
+    setContact({ name: "", email: "" });
+    navigate("/");
+  };
 
-    const editContactHandler=(contact,id)=>{
-        putcontact(id,contact)  
-        .then((res)=>getcontact())        
-        .catch()
-       };
+  const editContactHandler = (contact, id) => {
+    putcontact(id, contact)
+      .then((res) => getcontact())
+      .catch();
+  };
 
-    return ( 
-        <form onSubmit={submitform}>
-            <div className="formcontrol">
-                <label>name</label>
-                <input 
-                type='text' 
-                name='name' 
-                value={contact.name} 
-                onChange={changeHandler}  />
-            </div>
-            <div className="formcontrol">
-                <label>email</label>
-                <input 
-                type='text' 
-                name='email' 
-                value={contact.email} 
-                onChange={changeHandler}  />
-            </div>
-            <button type="submite">Edit</button>
+  return (
+    <form onSubmit={submitform}>
+      <div className="formcontrol">
+        <label>name</label>
+        <input
+          type="text"
+          name="name"
+          value={contact.name}
+          onChange={changeHandler}
+        />
+      </div>
+      <div className="formcontrol">
+        <label>email</label>
+        <input
+          type="text"
+          name="email"
+          value={contact.email}
+          onChange={changeHandler}
+        />
+      </div>
+      <button type="submite">Edit</button>
+      <Link to="/">go to contact list</Link>
+    </form>
+  );
+};
 
-
-
-
-            <Link to='/'>go to contact list</Link>
-        </form>
-     );
-}
- 
 export default EditContact;
